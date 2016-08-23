0c91c01d9467d001c8d488839c63a1796942f6fb
michaelisprihanto <michael.isprihanto@gmail.com>
2016-08-23 21:56:53 +0700

    Initial Commit

---
+++ ios-scrollview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-scrollview
+//
+//  Created by Michael Isprihanto on 8/23/16.
+//  Copyright © 2016 Openspace Digital Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
